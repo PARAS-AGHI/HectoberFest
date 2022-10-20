dd3f509f408a11c243c521ace5dc482f795cc15b
MyAcademy Classes <vavadiyagautam321@gmail.com>
2022-10-20 21:37:37 +0530

    BINARYSEARCH PROGRAM

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -11,14 +11,14 @@ write a program of BinarySearch
     cout.tie(NULL)
 #define clean fflush(stdin)
 using namespace std;
-int BinarySearch(int arr[], int x) // Binary Function Implementation through Iteration method
+int BinarySearch(int arr[], int x, int size) // Binary Function Implementation through Iteration method
 {
-    int l = 0;          // lower iterator
-    int e = arr.size(); // higher iterator
-    int mid = 0;        // mid index
-    while (l == e)
+    int l = 0;        // lower iterator
+    int e = size - 1; // higher iterator
+    while (l <= e)
     {
-        mid = (l + e) / 2; // assign value to mid
+
+        int mid = (l + e) / 2; // assign value to mid
         if (x == arr[mid])
         {
             return mid; // given number get so return index
@@ -32,14 +32,14 @@ int BinarySearch(int arr[], int x) // Binary Function Implementation through Ite
             l = mid + 1; // given number is greater than number of mid index in array so assign new value to lower
         }
     }
+    return -1; // not present in array
 }
-return -1; // not present in array
-}
+
 int main()
 {
-    int x;                                 // this is number you want to find in the
-    cin >> x;                              // take number form user
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 23}; // array
-    cout << binarysearch(arr, x);          // call binaryFunction
+    int x;                                                     // this is number you want to find in the
+    cin >> x;                                                  // take number form user
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 23};                     // array
+    cout << "In array Index at : " << BinarySearch(arr, x, 8); // call binaryFunction 8 is size of array
     return 0;
 }
